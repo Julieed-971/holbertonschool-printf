019d7e05dec4d1731fefa08c26e62d428726af4d
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-23 08:06:31 -0800

    Functions that prints integers and decimals

--- 1-get_functions.c
+++ 1-get_functions.c
@@ -9,33 +9,37 @@
 
 int spec_deci(va_list args)
 {
-        int num = va_arg(args, int);
-        int i = 0;
-        int digit_number = 0;
-        int digit_string[21];
+	int num = va_arg(args, int);
+	int i = 0;
+	int digit_number = 0;
+	int digit_string[21];
+	unsigned int abs = 0;
 
-        if (num == 0)
-        {
-                _putchar('0');
-                return (0);
-        }
-        if (num < 0)
-        {
-                _putchar('-');
-                num *= -1;
-                digit_number++;
-        }
-        while (num != 0)
-        {
-                digit_string[i] = (num % 10);
-                num /= 10;
-                i++;
-        }
-        while (i > 0)
-        {
-          _putchar(digit_string[i - 1] + '0');
-          digit_number++;
-          i--;
-        }
-        return (digit_number);
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+	if (num < 0)
+	{
+		abs = num * -1;
+		_putchar('-');
+		digit_number++;
+	}
+	if (num > 0)
+		abs = num;
+	while (abs != 0)
+	{
+		digit_string[i] = (abs % 10);
+		abs /= 10;
+		i++;
+	}
+	while (i > 0)
+	{
+		_putchar(digit_string[i - 1] + '0');
+		digit_number++;
+		i--;
+	}
+	return (digit_number);
 }
+
