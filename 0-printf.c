fcf10a538c73a9b5584500dd6975c8409e36e1ad
Julieed-971 <julie.dedieu1@gmail.com>
2023-11-24 05:54:55 -0800

    Simplified version of printf

--- 0-printf.c
+++ 0-printf.c
@@ -19,6 +19,8 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
+			if (format[i + 1] == '\0')
+				return (-1);
 			while (spec_list[j].c != '\0')
 			{
 				if (format[i + 1] == spec_list[j].c)
